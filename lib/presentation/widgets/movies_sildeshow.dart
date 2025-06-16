634a1d418ed4fe165a81f6b30987e1c581ed704d
@specktro <specktro@nonull.mx>
2025-06-15 22:47:21 -0600

    feat: Enhance movie slideshow with animations and styling
    - Add `animate_do` package for animations.
    - Implement `FadeIn` animation for movie images in the slideshow.
    - Add box shadows and rounded corners to movie slides.
    - Implement custom pagination dots for the slideshow using theme colors.

--- lib/presentation/widgets/movies_sildeshow.dart
+++ lib/presentation/widgets/movies_sildeshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/Movie.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,8 @@ class MoviesSlideWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 200,
       width: double.infinity,
@@ -19,6 +22,13 @@ class MoviesSlideWidget extends StatelessWidget {
         viewportFraction: 0.8,
         scale: 0.9,
         autoplay: true,
+        pagination: SwiperPagination(
+          margin: const EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
+        ),
         itemCount: movies.length,
         itemBuilder: (context, index) {
           return _Slide(movie: movies[index]);
@@ -36,6 +46,39 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+            color: Colors.black45,
+            blurRadius: 10,
+            offset: Offset(0, 10)
+        )
+      ]
+
+    );
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12)
+                );
+              }
+
+              return FadeIn(child: child);
+            },
+          )
+        )
+      ),
+    );
   }
 }

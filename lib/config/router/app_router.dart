41d9b002e5b9b453df7cd4ee2b8e47281ecdd34b
specktro <miguelangel.gomez@globant.com>
2025-06-18 12:56:10 -0600

    refactor: Refactor routing structure to nest movie screen route under home screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -8,15 +8,17 @@ final approuter = GoRouter(
     GoRoute(
       path: '/',
       name: 'home-screen',
-      builder: (context, state) => const HomeScreen()
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: 'movie-screen',
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no_id';
+            return MovieScreen(movieId: movieId);
+          }
+        )
+      ]
     ),
-    GoRoute(
-      path: '/movie/:id',
-      name: 'movie-screen',
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no_id';
-        return MovieScreen(movieId: movieId);
-      }
-    )
   ]
 );

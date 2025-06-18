c96cf8abb9e86ec8c95a4b8bb5c5b1ee9bc275c0
specktro <miguelangel.gomez@globant.com>
2025-06-18 17:47:47 -0600

    feat: Implement MovieInfoProvider for movie details
    - Create `movieDetailProvider` (StateNotifierProvider) to manage a map of movie details, keyed by movie ID.
    - Implement `MovieMapNotifier` to load movie details asynchronously.
    - Introduce a `GetMovieCallback` typedef for the movie fetching function.
    - Update `MovieScreen` to be a `ConsumerStatefulWidget`.
    - In `MovieScreen`, load movie details using `movieDetailProvider` in `initState`.
    - Display a loading indicator in `MovieScreen` while movie details are being fetched.

---
+++ lib/presentation/providers/movie_info_provider.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieDetailProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+  final moviesRepository = ref.watch(movieRespositoryProvider);
+  return MovieMapNotifier(getMovie: moviesRepository.getMovieById);
+});
+
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+  MovieMapNotifier({required this.getMovie}): super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) {
+      return;
+    }
+    // FIXME: This delay is just for demonstration purposes.
+    await Future.delayed(const Duration(milliseconds: 600));
+
+    final movie = await getMovie(movieId);
+    state = {...state, movieId: movie};
+  }
+}
--- lib/presentation/screens/movie_screen.dart
+++ lib/presentation/screens/movie_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cinemapedia/presentation/providers/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   final String movieId;
   
   const MovieScreen({
@@ -9,10 +12,27 @@ class MovieScreen extends StatelessWidget {
   });
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(movieDetailProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieDetailProvider)[widget.movieId];
+
+    if (movie == null) {
+      return Scaffold(body: const Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie ID: $movieId'),
+        title: Text('Movie ID: ${widget.movieId}'),
       ),
       body: const Placeholder()
     );

c50c7bd6d945254ef9fab9db169593d43ae0e808
specktro <specktro@nonull.mx>
2025-06-21 16:19:41 -0600

    feat: Update movie and movie details models to handle nullable release dates and improve search functionality in the app with a no debounce approach.

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,16 +1,33 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+typedef SearchMoviescallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviescallback onSearch;
+
+  SearchMovieDelegate({required this.onSearch});
+
+  @override
+  String get searchFieldLabel => 'Search movie';
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text('buildActionsData'),
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(onPressed: () => query = '', icon: Icon(Icons.clear)),
+      ),
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(Icons.arrow_back_ios_new_rounded),
+    );
   }
 
   @override
@@ -20,7 +37,19 @@ class SearchMovieDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('buildSuggestions');
-  }
+    return FutureBuilder(
+      future: onSearch(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
 
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(title: Text(movie.title));
+          },
+        );
+      },
+    );
+  }
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,12 @@
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
@@ -22,14 +24,20 @@ class CustomAppBar extends StatelessWidget {
               const Spacer(),
               IconButton(
                 onPressed: () {
-                  showSearch(context: context, delegate: SearchMovieDelegate());
+                  final movieRepository = ref.read(movieRespositoryProvider);
+                  showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      onSearch: movieRepository.searchMovies,
+                    ),
+                  );
                 },
-                icon: const Icon(Icons.search)
-              )
-            ]
-          )
+                icon: const Icon(Icons.search),
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
